21aaa568f0a2c87ca7d4bdd8e89ee46e2681b19d
Uilton Santos <uilton.jose@gmail.com>
2019-03-11 22:56:37 +0000

    Improvement/cipher util (#8)
    * - New APIs util for encrypt/decrypt
    
    * Improvement on encrypt method util.

--- src/routes/api/util.js
+++ src/routes/api/util.js
@@ -3,6 +3,7 @@ const customerController = require("../../controller/customerController");
 const express = require("express");
 const router = express.Router();
 const { handleResult } = require("../../utils/APIUtil");
+const encryptUtil = require("../../utils/EncryptUtil");
 
 router.get("/users", (req, res) => {
   userController.listAllUsers(result => {
@@ -21,6 +22,18 @@ router.get("/synchronizeCustomersWithProviders", (req, res) => {
   customerController.synchronizeCustomersWithProviders(result => {
     handleResult(result, res);
   });
+});
+
+router.get("/encrypt", (req, res) => {
+  const value = req.query.valueToEncrypt;
+  const result = encryptUtil.encryptString(value);
+  res.json(result);
+});
+
+router.get("/decrypt", (req, res) => {
+  const cipher = req.query.cipher;
+  const result = encryptUtil.decryptString(cipher);
+  res.json(result);
 });
 
 module.exports = router;
