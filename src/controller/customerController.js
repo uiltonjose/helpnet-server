1b97f97421cad422bef89611ae96bf717508212a
julio <julionevess@gmail.com>
2019-04-21 15:21:20 -0300

    Criação da classe FileUtil.js Organização do código Utilização da API AWS

--- src/controller/customerController.js
+++ src/controller/customerController.js
@@ -3,8 +3,7 @@ const customerDAO = require("../dao/customerDAO"),
   dateUtil = require("../utils/DateUtil"),
   StringUtil = require("../utils/StringUtil"),
   StatusCode = require("../utils/StatusCode"),
-  fs = require("fs"),
-  JSZip = require("jszip");
+  FileUtil = require("../utils/FileUtil.js");
 
 const _ = require("lodash");
 
@@ -281,21 +280,23 @@ const syncronizedCustomersFromFile = providerID => {
             customersFromHelpnet
           );
         }
-        getCustomersFromFile(providerID).then(customersFromProvider => {
-          if (
-            typeof customersFromProvider !== "undefined" &&
-            typeof customersFromProvider[0] !== "undefined"
-          ) {
-            synchronizeCustomer(
-              customersFromProvider,
-              customersFromHelpnet,
-              providerID,
-              resolve
-            );
-          } else {
-            resolve("Arquivo não encontrado ou arquivo vazio.");
+        FileUtil.getCustomersFromFile(providerID).then(
+          customersFromProvider => {
+            if (
+              typeof customersFromProvider !== "undefined" &&
+              typeof customersFromProvider[0] !== "undefined"
+            ) {
+              synchronizeCustomer(
+                customersFromProvider,
+                customersFromHelpnet,
+                providerID,
+                resolve
+              );
+            } else {
+              resolve("Arquivo não encontrado ou arquivo vazio.");
+            }
           }
-        });
+        );
       },
       error => {
         handleFailRequest(error, resolve);
@@ -409,39 +410,6 @@ const listCustomersByProviderId = providerId => {
   });
 };
 
-const getCustomersFromFile = providerID => {
-  return new Promise(resolve => {
-    try {
-      /*
-    const fileName = providerID + "_" + dateUtil.getDateToFileName() + ".maz";
-    const data = fs.readFileSync(fileName, "utf8");
-    JSZip.loadAsync(data).then(function(zip) {
-      files = Object.keys(zip.files);
-      console.log(files);
-    });
-*/
-      const fileName = providerID + "_" + dateUtil.getDateToFileName() + ".zip";
-      const data = fs.readFileSync(fileName);
-      JSZip.loadAsync(data).then(function(zip) {
-        files = Object.keys(zip.files);
-        console.log(files);
-        zip
-          .file(files[0])
-          .async("string")
-          .then(function(data) {
-            // data is "Hello World!"
-            //console.log("data");
-            resolve(builderListCustomerFromFile(data));
-          });
-
-        //const data = zip.files[files[0]].date;
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  });
-};
-
 module.exports = {
   getProviderByCustomerCpfCnpjAndProviderCod: getProviderByCustomerCpfCnpjAndProviderCod,
   synchronizeCustomersWithProviders: synchronizeCustomersWithProviders,
@@ -451,45 +419,3 @@ module.exports = {
   syncCustomersFromFiles: syncCustomersFromFiles,
   syncCustomersFromFilesByProviderId: syncCustomersFromFilesByProviderId
 };
-
-function builderListCustomerFromFile(data) {
-  let customers = [];
-  var linhas = data.split("\n");
-  var insert = "";
-  var i = 0;
-  while (i < linhas.length && insert === "") {
-    if (linhas[i].substring(0, 34) === "INSERT INTO `sis_cliente` VALUES (") {
-      insert = linhas[i].substring(34);
-    }
-    i++;
-  }
-  i = 0;
-  var rows = insert.split(",(");
-  rows.forEach(row => {
-    row = row.replace(/'/g, "");
-    var itens = row.split(",");
-    let customer = {};
-    customer.id = itens[0];
-    customer.nome = itens[1];
-    customer.cpf_cnpj = itens[8];
-    customer.nome_res = itens[79];
-    customer.fone = itens[9];
-    customer.celular = itens[26];
-    customer.login = itens[14];
-    customer.email = itens[2];
-    customer.endereco = itens[3];
-    customer.numero = itens[44];
-    customer.complemento = itens[21];
-    customer.bairro = itens[4];
-    customer.cidade = itens[5];
-    customer.estado = itens[7];
-    customer.cep = itens[6];
-    customer.bloqueado = itens[27];
-    customer.cli_ativado = itens[34];
-    customer.cadastro = itens[13];
-    customer.plano = itens[32];
-    customers.push(customer);
-    i = i + 1;
-  });
-  return customers;
-}
---
+++ src/utils/FileUtil.js
@@ -0,0 +1,95 @@
+const fs = require("fs"),
+  JSZip = require("jszip"),
+  dateUtil = require("../utils/DateUtil"),
+  AWS = require("aws-sdk");
+
+require("dotenv").config();
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const getFileFromAWS = fileName => {
+  return new Promise(resolve => {
+    var s3 = new AWS.S3();
+    s3.getObject({ Bucket: "providerbackup", Key: fileName }, function(
+      error,
+      data
+    ) {
+      if (error != null) {
+        console.log(error);
+      } else {
+        resolve(data.Body);
+      }
+    });
+  });
+};
+
+const getCustomersFromFile = providerID => {
+  return new Promise(resolve => {
+    try {
+      const fileName = providerID + "_" + dateUtil.getDateToFileName() + ".zip";
+      getFileFromAWS(fileName).then(function(data) {
+        JSZip.loadAsync(data).then(function(zip) {
+          files = Object.keys(zip.files);
+          console.log(files);
+          zip
+            .file(files[0])
+            .async("string")
+            .then(function(data) {
+              resolve(builderListCustomerFromFile(data));
+            });
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+function builderListCustomerFromFile(data) {
+  let customers = [];
+  var linhas = data.split("\n");
+  var insert = "";
+  var i = 0;
+  while (i < linhas.length && insert === "") {
+    if (linhas[i].substring(0, 34) === "INSERT INTO `sis_cliente` VALUES (") {
+      insert = linhas[i].substring(34);
+    }
+    i++;
+  }
+  i = 0;
+  var rows = insert.split(",(");
+  rows.forEach(row => {
+    row = row.replace(/'/g, "");
+    var itens = row.split(",");
+    let customer = {};
+    customer.id = itens[0];
+    customer.nome = itens[1];
+    customer.cpf_cnpj = itens[8];
+    customer.nome_res = itens[79];
+    customer.fone = itens[9];
+    customer.celular = itens[26];
+    customer.login = itens[14];
+    customer.email = itens[2];
+    customer.endereco = itens[3];
+    customer.numero = itens[44];
+    customer.complemento = itens[21];
+    customer.bairro = itens[4];
+    customer.cidade = itens[5];
+    customer.estado = itens[7];
+    customer.cep = itens[6];
+    customer.bloqueado = itens[27];
+    customer.cli_ativado = itens[34];
+    customer.cadastro = itens[13];
+    customer.plano = itens[32];
+    customers.push(customer);
+    i = i + 1;
+  });
+  return customers;
+}
+
+module.exports = {
+  getCustomersFromFile: getCustomersFromFile
+};
